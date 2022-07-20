3ffa0d265085eab63904ffcc4ff8e04f334b2aeb
zPG0D <pedromarininave123@gmail.com>
2022-07-20 15:30:20 -0300

    Changes

--- src/main/java/com/zpedroo/farmsword/listeners/PointsListeners.java
+++ src/main/java/com/zpedroo/farmsword/listeners/PointsListeners.java
@@ -36,11 +36,12 @@ public class PointsListeners implements Listener {
 
         event.setCancelled(true);
 
-        cursor.setAmount(cursor.getAmount() - 1);
-        event.setCursor(cursor);
+        final int itemAmount = cursor.getAmount();
+        event.setCursor(new ItemStack(Material.AIR));
 
         int pointsAmount = cursorNBT.getInteger(FarmSwordUtils.POINTS_ITEM_NBT);
-        ItemStack newItem = FarmSwordUtils.addItemPoints(item, pointsAmount);
+        int finalPointsAmount = pointsAmount * itemAmount;
+        ItemStack newItem = FarmSwordUtils.addItemPoints(item, finalPointsAmount);
 
         player.getInventory().setItem(event.getSlot(), newItem);
         player.playSound(player.getLocation(), Sound.LEVEL_UP, 1f, 10f);
